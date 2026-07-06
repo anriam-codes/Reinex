8d43a2f70b169e95469624a2e4851e85cf9de45e
Krithik <krithik.sriharsha@gmail.com>
2026-07-06 23:53:12 +0530

    feat: add dbt tests

--- reinex_dbt/models/marts/dimensions/dim_time.sql
+++ reinex_dbt/models/marts/dimensions/dim_time.sql
@@ -2,9 +2,9 @@ select distinct
     pickup_datetime::date as date_key,
 
     extract(year from pickup_datetime) as year,
+    extract(quarter from pickup_datetime) as quarter,
     extract(month from pickup_datetime) as month,
     extract(day from pickup_datetime) as day,
-    extract(hour from pickup_datetime) as hour,
 
     extract(dow from pickup_datetime) as weekday,
 
